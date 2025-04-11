1e262841145cd8ec1453bb620635805e71c53853
ramandeepjain <ramandeepjain@gmail.com>
2025-04-11 16:20:23 +0200

    run on different port while developing

--- reservation_portal/vite.config.js
+++ reservation_portal/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5172,
+    host: 'localhost' // Optional: change to '0.0.0.0' if you want LAN access
+  }
 })
