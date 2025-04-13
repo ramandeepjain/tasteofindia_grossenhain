96ba72272c5531fcd9a3db39ae90d2d5fe5e2fcd
ramandeepjain <ramandeepjain@gmail.com>
2025-04-13 11:33:00 +0200

    allow host

--- reservation_portal/vite.config.js
+++ reservation_portal/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5172,
-    host: 'localhost' // Optional: change to '0.0.0.0' if you want LAN access
+    host: 'localhost', // Optional: change to '0.0.0.0' if you want LAN access
+    allowedHosts: ['reservations.tasteofindia-grossenhain.de']
   }
 })
